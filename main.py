05cf89aeee05a28b6809667a3afaf9e2ef7335c1
chokinski <chokinski702@gmail.com>
2023-09-10 17:25:17 -0400

    .

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ root = Tk()
 root.title('HI BUTTON')
 root.geometry('500x500') 
 
-button = Button(root, text='CLICK :):)', command= 'HI')
-button.pack()
+
+
 
 btnOne = Button(root, text="Click this!", bd='4',
                 command = root.destroy)
