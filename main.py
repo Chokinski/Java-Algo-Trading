adf41c871e38d985a61027dc3f6fe8372fbab4ce
chokinski <chokinski702@gmail.com>
2023-09-10 17:17:21 -0400

    Changed pams

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from tkinter import *
 
 root = Tk()
 root.title('HI BUTTON')
-root.geometry('1000x1000') 
+root.geometry('500x500') 
 
 button = Button(root, text='CLICK :):)', command= 'HI')
 button.pack()
