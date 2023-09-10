d3fab199110293e8a5e06c1c56fad53780026996
chokinski <chokinski702@gmail.com>
2023-09-10 18:39:19 -0400

    Created Show and Hide Button.

--- main.py
+++ main.py
@@ -1,14 +1,40 @@
 from tkinter import *
 
-root = Tk()
-root.title('HI BUTTON')
-root.geometry('500x500') 
+def showText():
+    global txtState
+    if txtState == False:
+        label.pack()
+        btnOne.pack_forget()
+        btnTwo.pack(side ='bottom')
+    txtState = True
 
-label = Label(root, text='HI')
-label.pack(y=30)
+def hideText():
+    global txtState
+    if txtState == True:
+        label.pack_forget()
+        btnTwo.pack_forget()
+        btnOne.pack(side= 'top')
+    txtState = False
 
-btnOne = Button(root, text="Click this!", bd='4',
-                command = )
-btnOne.pack(side= 'top')
 
-root.mainloop()
+def main():
+    global txtState
+    global btnOne
+    global btnTwo
+    global label
+    root = Tk()
+    root.title('HI BUTTON')
+    root.geometry('500x500') 
+    label = Label(root, text="Hi")
+    
+
+    txtState = False
+    btnOne = Button(root,justify='center', text="Show this!", bd='2',
+                    command = lambda: showText())
+
+    btnTwo = Button(root,justify='center', text="Hide this!", bd='4',
+                    command = lambda: hideText())
+    btnOne.pack(side= 'top')
+
+    root.mainloop()
+main()
