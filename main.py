1dc625b2df8ae6fca8f72df7ff965c61d9a4fe9a
DragonSlayer727 <rovers1555@gmail.com>
2023-09-10 17:09:58 -0400

    new

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 from tkinter import *
 
 root = Tk()
+root.title('HI BUTTON')
+root.geometry('1000x1000') 
 
-root.geometry = ('1000*1000')
+button = Button(root, text='CLICK :):)', command= 'HI')
+button.pack()
 
 root.mainloop()
