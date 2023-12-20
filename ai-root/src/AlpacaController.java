6790c363f0714f5a01401ecbc2be094aa14039e8
chokinski <chokinski702@gmail.com>
2023-12-20 18:22:32 -0500

    Add new JAR dependencies to project

--- ai-root/src/AlpacaController.java
+++ ai-root/src/AlpacaController.java
@@ -1,16 +1,33 @@
 package src;
-import okhttp3.Interceptor;
-import okhttp3.OkHttpClient;
+
 import net.jacobpeterson.alpaca.AlpacaAPI;
 import net.jacobpeterson.alpaca.model.endpoint.account.Account;
 import net.jacobpeterson.alpaca.rest.AlpacaClientException;
-
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
 public class AlpacaController {
     private static AlpacaAPI alpaca;
 
+    /**
+     * Connects to the Alpaca API using the properties specified in the "alpaca.properties" file.
+     * If the file or path is not found, a FileNotFoundException is thrown.
+     * If an IO exception occurs while loading the properties, an IOException is thrown.
+     */
     public static void connect()
     {
-        AlpacaAPI alpaca = new AlpacaAPI();
+        Properties properties = new Properties();
+        try {
+            properties.load(new FileInputStream("src\\alpaca.properties"));
+            alpaca = new AlpacaAPI();
+            
+        } catch (FileNotFoundException exception) {
+            ai.botLogger.error("Erorr, File/Path Not Found:"+ exception.getMessage());
+        } catch (IOException exception) {
+            ai.botLogger.error("Erorr, IO Exception:"+ exception.getMessage());
+        }
+        
     }
 
     public static Account getAccount() {
@@ -24,7 +41,6 @@ public class AlpacaController {
                 return null;
     }
     
-
 }
 
 
