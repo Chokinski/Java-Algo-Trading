c3366ab976d93a9fb3dbf85263c83bf41a67a4a2
chokinski <chokinski702@gmail.com>
2024-01-03 01:48:45 -0500

    test ui

--- ai-root/src/JAT/Controller.java
+++ ai-root/src/JAT/Controller.java
@@ -1,14 +1,21 @@
 package JAT;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 
 
 public class Controller {
+
+
+    
+    @FXML
+    private Button btnLightMode;
 
     @FXML
     private Button btnLogin;
@@ -19,6 +26,16 @@ public class Controller {
     @FXML
     private PasswordField tfSec_ID;
 
+    @FXML
+    
+    void btnLightSwitch(ActionEvent event) {
+        
+        double opacity = mainWindow.getOpacity() == 1.0 ? 0.2 : 1.0;
+        mainWindow.setOpacity(opacity);
+        String btnText = btnLightMode.getText().equals("Light") ? "Dark" : "Light";
+        btnLightMode.setText(btnText);
+
+    }
     @FXML
     void onBtnClick(ActionEvent event) {
         String title = tfKey_ID.getText();
@@ -31,3 +48,5 @@ public class Controller {
         this.mainWindow = mainWindow;
     }
 }
+
+
